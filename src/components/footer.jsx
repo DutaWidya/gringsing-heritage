fb39607e1db88e6244d92ecd645e47790447cf3c
dutawidya <dutaawidya19@gmail.com>
2024-04-16 19:05:34 +0800

    feat: add responsive navbar

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,43 @@
+import { useLocation, Link } from "react-router-dom";
+import { navlinks } from "../config";
+import { useState } from "react";
+
 export const Footer = () => {
+    const location = useLocation();
+    const [open, setOpen] = useState(false);
+    const [navigations] = useState([...navlinks, { id: 6, name: "Kontak", href: "/kontak" }]);
+
+    const handleNavbar = () => {
+        setOpen((isOpen) => !isOpen);
+    };
     return (
-        <div className="container mx-auto py-3 px-12 flex items-center justify-center w-full font-medium">
-            <span>© Gringsing Heritage. All Rights Reserved</span>
+        <div className="hidden md:flex container mx-auto px-4 md:px-12 flex-col items-center justify-center w-full font-medium">
+            <div className="w-full flex items-center border-y border-[#2d2824] py-4">
+                <ul
+                    className={
+                        `absolute bg-white flex flex-col gap-5 flex-auto items-center justify-center md:relative md:flex-row transition-all ease-in-out duration-1000 ` +
+                        `${
+                            open
+                                ? "top-0 left-0 right-0 bottom-0"
+                                : "top-0 -left-[100vw] right-[100vw] bottom-0 md:left-0 md:right-0"
+                        }`
+                    }
+                >
+                    {navigations.map((nav) => (
+                        <Link
+                            to={nav.href}
+                            className={`nav-item ${
+                                location.pathname === nav.href ? "active" : ""
+                            } `}
+                            key={nav.id}
+                            onClick={handleNavbar}
+                        >
+                            <li>{nav.name}</li>
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <span className="py-4">© Gringsing Heritage. All Rights Reserved</span>
         </div>
     );
 };
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,2 +1,4 @@
 export * from "./navigation-bar";
 export * from "./footer";
+export * from "./section";
+export * from "./card";
