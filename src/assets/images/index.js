114df30f83b7701409480aa82b50a94077f09181
dutawidya <dutaawidya19@gmail.com>
2024-04-19 22:19:47 +0800

    feat: add animation to galery card

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,6 @@
 import Slide1 from "./slide_1.png";
 import Slide2 from "./slide_2.png";
 import Slide3 from "./slide_3.png";
+import BG_3 from "./BG_3.webp";
 
-export { Slide1, Slide2, Slide3 };
+export { Slide1, Slide2, Slide3, BG_3 };
